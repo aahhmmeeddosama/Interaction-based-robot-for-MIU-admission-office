31c8928fb4a3c317af1d94222d581a203c5ba710
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2022-03-08 22:24:29 +0200

    Update

--- android_robot/lib/main.dart
+++ android_robot/lib/main.dart
@@ -2,24 +2,17 @@ import 'package:android_robot/admission_screen.dart';
 import 'package:android_robot/chatbot.dart';
 import 'package:flutter/material.dart';
 
-import 'ddddd.dart';
 import 'identify_user.dart';
 import 'welcome_screen.dart';
-import 'zzzzzzzzz.dart';
-
 
 Future<void> main() async {
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'MIU ADMISSION',
       //home: TTS(),
       //home: SpeechScreen(),
@@ -31,7 +24,6 @@ class MyApp extends StatelessWidget {
       // home: Add(),
       //home: Delete(),
       //home: Edit(),
-
     );
   }
 }
