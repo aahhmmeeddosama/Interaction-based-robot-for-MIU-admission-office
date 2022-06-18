5e283aa6dd1828829fb9e955f385e3b7602c62f9
Mafdy Magdy <64131892+mafdymagdy@users.noreply.github.com>
2022-06-18 14:33:17 +0200

    Update

--- android_robot/lib/edit_from_dataset.dart
+++ android_robot/lib/edit_from_dataset.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'constants/app_color.dart';
 import 'API.dart';
-  
-class Edit_Q_A extends StatefulWidget {
+
+class EditFromDataset extends StatefulWidget {
   String URL;
-  Edit_Q_A(this.URL);
-  @override  
+  EditFromDataset(this.URL);
+  @override
   _State createState() => _State(URL);
 }
 
-class _State extends State<Edit_Q_A> {
-  var formKey = GlobalKey<FormState>();
-
-  @override
+class _State extends State<EditFromDataset> {
   String URL;
   String? url;
   String? t;
   var Data;
-
   final myController1 = TextEditingController();
   final myController2 = TextEditingController();
   final myController3 = TextEditingController();
   final myController4 = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   _State(this.URL);
-
-
 
   @override
   void dispose() {
@@ -34,47 +29,47 @@ class _State extends State<Edit_Q_A> {
     myController2.dispose();
     myController3.dispose();
     myController4.dispose();
-
     super.dispose();
   }
 
-  Widget build(BuildContext context) {  
-    return Scaffold(  
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text('Edit Question'),
-          backgroundColor: Colors.red[700],
-        ),  
+          title: Text('Edit Question', style: TextStyle(fontSize: 30)),
+          backgroundColor: MyColors.myRed,
+        ),
         body: SingleChildScrollView(
           child: Form(
             key: formKey,
             child: Padding(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.fromLTRB(40, 80, 40, 80),
                 child: Column(
                   children: <Widget>[
-                     Text(
-                        'enter questions\' index then edited data',
-                       textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontFamily: "Times New Roman",
-                          fontWeight: FontWeight.bold,
-                        ),
+                    Text(
+                      'Enter questions\' index then edited data',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: MyColors.myBlack,
+                        fontFamily: "Times New Roman",
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
                     Padding(
                       padding: EdgeInsets.all(20),
                       child: TextFormField(
                         controller: myController1,
-
-
                         decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(borderRadius: new BorderRadius.circular(25.0),
-                            borderSide:  BorderSide(color: Colors.red ),),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: BorderSide(color: MyColors.myRed),
+                          ),
                           labelText: 'Index',
                           hintText: 'Write Index',
                           labelStyle: TextStyle(
-                            color: Colors.black54,
+                            color: MyColors.myBlack,
+                            fontSize: 30,
                           ),
                         ),
                         validator: (value) {
@@ -92,15 +87,17 @@ class _State extends State<Edit_Q_A> {
                       padding: EdgeInsets.all(20),
                       child: TextFormField(
                         controller: myController2,
-
 //                      url +='&tag=' + value.toString();
                         decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(borderRadius: new BorderRadius.circular(25.0),
-                            borderSide:  BorderSide(color: Colors.red ),),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
                           labelText: 'Tag',
                           hintText: 'Edit Tag',
                           labelStyle: TextStyle(
-                            color: Colors.black54,
+                            color: MyColors.myBlack,
+                            fontSize: 30,
                           ),
                         ),
                         validator: (value) {
@@ -118,14 +115,16 @@ class _State extends State<Edit_Q_A> {
                       padding: EdgeInsets.all(20),
                       child: TextFormField(
                         controller: myController3,
-
                         decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(borderRadius: new BorderRadius.circular(25.0),
-                            borderSide:  BorderSide(color: Colors.red ),),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: BorderSide(color: MyColors.myRed),
+                          ),
                           labelText: 'Pattern',
                           hintText: 'Edit Pattern',
                           labelStyle: TextStyle(
-                            color: Colors.black54,
+                            color: MyColors.myBlack,
+                            fontSize: 30,
                           ),
                         ),
                         validator: (value) {
@@ -139,17 +138,20 @@ class _State extends State<Edit_Q_A> {
                         },
                       ),
                     ),
-                     Padding(
-                       padding: EdgeInsets.all(20),
+                    Padding(
+                      padding: EdgeInsets.all(20),
                       child: TextFormField(
                         controller: myController4,
                         decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(borderRadius: new BorderRadius.circular(25.0),
-                            borderSide:  BorderSide(color: Colors.red ),),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: BorderSide(color: MyColors.myRed),
+                          ),
                           labelText: 'Response',
                           hintText: 'add Response',
                           labelStyle: TextStyle(
-                            color: Colors.black54,
+                            color: MyColors.myBlack,
+                            fontSize: 30,
                           ),
                         ),
                         validator: (value) {
@@ -163,49 +165,48 @@ class _State extends State<Edit_Q_A> {
                         },
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.all(50),
                       child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.red[700], // background
-                                onPrimary: Colors.white, // foreground
-                                 minimumSize: Size(200,50),
-                                shape: StadiumBorder(),
-
-                              ),
+                        style: ElevatedButton.styleFrom(
+                          primary: MyColors.myRed, // background
+                          onPrimary: MyColors.myWhite, // foreground
+                          minimumSize: Size(200, 70),
+                          shape: StadiumBorder(),
+                        ),
                         onPressed: () async {
                           if (formKey.currentState!.validate()) {
                             myController1;
                             myController2;
                             myController3;
                             myController4;
-                            String url = URL + 'edit?edit=' + myController1.text +
-                                '&tag=' + myController2.text + '&pattern=' +
-                                myController3.text + '&response=' +
-                                myController4.text + '&context=kllkj';
+                            String url = URL +
+                                'edit?edit=' +
+                                myController1.text +
+                                '&tag=' +
+                                myController2.text +
+                                '&pattern=' +
+                                myController3.text +
+                                '&response=' +
+                                myController4.text +
+                                '&context=kllkj';
                             Data = await Getdata(url);
                             myController1.clear();
                             myController2.clear();
                             myController3.clear();
                             myController4.clear();
-
-
                             return setState(() {});
-
                             /*setState(() {
                             QueryText = Data['delete'] ;
                           });*/
                           }
                         },
-                              child: Text('Submit'),
-                            ),
+                        child: Text('Submit', style: TextStyle(fontSize: 30)),
+                      ),
                     )
                   ],
-                )
-            ),
+                )),
           ),
-        )  
-    );  
-  }  
-} 
+        ));
+  }
+}
